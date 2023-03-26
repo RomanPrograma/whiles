4bb1e854210a307754f765b982298bde3052705d
RomanPrograma <126255710+RomanPrograma@users.noreply.github.com>
2023-03-26 10:22:22 +0300

    fix, done

--- whiles/whiles.py
+++ whiles/whiles.py
@@ -1,5 +1,7 @@
 begin = int(input("enter a begin  "))
 end = int(input("enter an end  "))
+if begin > end:
+    begin, end = end, begin
 for i in range(begin, end+1):
     if i %2 !=0:
         print(i)
