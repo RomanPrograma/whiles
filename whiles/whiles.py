a7be7d49238716db98f6afdf9a4420d7f03ccfd2
RomanPrograma <126255710+RomanPrograma@users.noreply.github.com>
2023-03-26 11:51:57 +0300

    ex_2

--- whiles/whiles.py
+++ whiles/whiles.py
@@ -1,1 +1,17 @@
-
+begin = int(input('enter begin  '))
+end = int(input('enter end  '))
+counter = 0
+for i in range(begin, end+1):
+    print(i)
+print('\n')
+for i in range(end, begin-1, -1):
+    print(i)
+print('\n')
+for i in range(begin, end+1):
+    if i %7 ==0:
+        print(i)
+print('\n')
+for i in range(begin, end+1):
+    if i %5 ==0:
+        counter += 1
+print(counter)
